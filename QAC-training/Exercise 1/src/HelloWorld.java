a41be3d148c3a01f7a5ad0e2905b2fd68584a122
premalnayee <premnayee1@gmail.com>
2020-05-04 14:44:52 +0100

    Initial commit for excercise 1

---
+++ QAC-training/Exercise 1/src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public String HelloWorld() {
+        return "Hello World";
+    }
+
+    public void Heya(String output){
+        System.out.println(output);
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
