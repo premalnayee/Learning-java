7ee1d6d0a5a073cd57859280b71d8fb92c441edb
premalnayee <premnayee1@gmail.com>
2020-05-04 15:23:27 +0100

    Initial commit for Exercise 3

---
+++ QAC-training/Exercise 3 - CheckString/src/CheckString.java
@@ -0,0 +1,25 @@
+public class CheckString {
+
+    String compare;
+    String WithMe;
+
+    public CheckString(String compare, String WithMe) {
+        this.compare = compare;
+        this.WithMe = WithMe;
+    }
+
+    boolean check_internal () {
+        var WithMe = "Potatoes";
+
+        return  compare == WithMe;
+    }
+
+    boolean compare() {
+        return compare == WithMe;
+    }
+
+    public static void main(String[] args) {
+        CheckString Potatoes = new CheckString("Potatoes",null        );
+        System.out.println(Potatoes.check_internal());
+    }
+}
