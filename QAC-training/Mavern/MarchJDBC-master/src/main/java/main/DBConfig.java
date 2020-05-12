5d7916726358fc4d8a823af430fda316a8c3cd8f
premalnayee <premnayee1@gmail.com>
2020-05-12 12:11:11 +0100

    Code from instructor

---
+++ QAC-training/Mavern/MarchJDBC-master/src/main/java/main/DBConfig.java
@@ -0,0 +1,12 @@
+package main;
+
+public final class DBConfig {
+
+	public static final String DB_URL = "jdbc:mysql://localhost/sakila";
+	public static final String USER = "root";
+	public static final String PASS = "root";
+
+	private DBConfig() {
+	}
+
+}
